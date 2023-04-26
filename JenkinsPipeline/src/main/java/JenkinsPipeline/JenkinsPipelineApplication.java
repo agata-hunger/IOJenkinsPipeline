6b3edd558e7f9cb3a1d2e97a42f36f92553ed67e
agata-hunger <72617977+agata-hunger@users.noreply.github.com>
2023-04-26 11:27:08 +0200

    initial commit
    # add project to respository

---
+++ JenkinsPipeline/src/main/java/JenkinsPipeline/JenkinsPipelineApplication.java
@@ -0,0 +1,19 @@
+package JenkinsPipeline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@SpringBootApplication
+public class JenkinsPipelineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsPipelineApplication.class, args);
+	}
+
+	@GetMapping
+	public String get() {
+		return "Hello Jenkins!";
+	}
+
+}
